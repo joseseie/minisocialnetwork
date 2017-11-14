1a9005bccc8bfa3e6831dd3cb962217fd86f445e
Jose Seie <joseeugenioseie@hotmail.com>
2017-11-14 05:22:06 +0200

    Depois  da remocao  das rotas de mensagens

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,3 @@ Route::get('/user/profile}','ProfileController@profile');
 
 Route::resource('articles','ArticlesController');
 
-// Messages
-Route::get('/direct-messages', 'MessagesController@index');
-Route::get('/direct-messages/show/{id}', 'MessagesController@index');
-Route::post('/direct-messages/chat', 'MessagesController@chat');
-Route::post('/direct-messages/send', 'MessagesController@send');
-Route::post('/direct-messages/new-messages', 'MessagesController@newMessages');
-Route::post('/direct-messages/people-list', 'MessagesController@peopleList');
-Route::post('/direct-messages/delete-chat', 'MessagesController@deleteChat');
-Route::post('/direct-messages/delete-message', 'MessagesController@deleteMessage');
-Route::post('/direct-messages/notifications', 'MessagesController@notifications');
